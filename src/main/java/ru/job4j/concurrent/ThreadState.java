419be03ff81a1b7f00ad7391f316ccd64817f21e
krugo <krugovenkov@mail.ru>
2024-02-27 11:01:34 +0300

    3. Состояние нити. [#229175 #482979]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -14,7 +14,7 @@ public class ThreadState {
         System.out.printf("Статус потока: %s = %s%s", second.getName(), second.getState(), System.lineSeparator());
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
             System.out.printf("Ожидание потоков... %s", System.lineSeparator());
         }
         System.out.printf("Статус потока: %s = %s%s", first.getName(), first.getState(), System.lineSeparator());
