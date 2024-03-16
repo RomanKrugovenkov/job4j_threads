ac1a286e2bf1c4b6a35e3aa9becbeea4610b645e
krugo <krugovenkov@mail.ru>
2024-03-16 21:51:50 +0300

    8. Скачивание файла с ограничением. [#144271 #483960]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -1,25 +1,28 @@
 package ru.job4j.thread;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Paths;
 
 public class Wget implements Runnable {
     private final String url;
+    private final String fileName;
     private final int speed;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, String fileName, int speed) {
         this.url = url;
+        this.fileName = fileName;
         this.speed = speed;
     }
 
     @Override
     public void run() {
         var startAt = System.currentTimeMillis();
-        var file = new File("tmp.xml");
         try (var input = new URL(url).openStream();
-             var output = new FileOutputStream(file)) {
+             var output = new FileOutputStream(fileName)) {
             System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
             var dataBuffer = new byte[512];
             int bytesRead;
@@ -28,19 +31,41 @@ public class Wget implements Runnable {
                 output.write(dataBuffer, 0, bytesRead);
                 var afterAt = System.nanoTime();
                 System.out.println("Read 512 bytes : " + (afterAt - beforeAt) + " nano.");
-                var sleepTime =  1000000 * dataBuffer.length / (afterAt - beforeAt) / speed;
-                System.out.println("Время задержки = " + sleepTime + " мс");
-                Thread.sleep(sleepTime);
+                var sleepTime = 1000000 * dataBuffer.length / (afterAt - beforeAt) / speed;
+                if (sleepTime > 1) {
+                    System.out.println("Время задержки = " + sleepTime + " мс");
+                    Thread.sleep(sleepTime);
+                }
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void validation(String[] args) {
+        boolean urlTrue;
+        try {
+            new URL(args[0]).toURI();
+            urlTrue = true;
+        } catch (MalformedURLException e) {
+            urlTrue = false;
+        } catch (URISyntaxException e) {
+            urlTrue = false;
+        }
+        if (!urlTrue) {
+            throw new IllegalArgumentException(String.format("URL is incorrect \"%s\"", Paths.get(args[0]).getFileName()));
+        }
+        if (Integer.parseInt(args[1]) < 1) {
+            throw new IllegalArgumentException(String.format("Speed is incorrect \"%s\"", args[1]));
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, MalformedURLException {
+        validation(args);
         String url = args[0];
+        String fileName = Paths.get(new URL(url).getPath()).getFileName().toString();
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        Thread wget = new Thread(new Wget(url, fileName, speed));
         System.out.println("Загрузка файла с ограничением скорости " + speed + " кб/с...");
         wget.start();
         wget.join();
