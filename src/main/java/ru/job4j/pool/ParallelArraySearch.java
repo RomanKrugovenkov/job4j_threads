b0a12f16c9833e4abd51877ede748e27ec11a5f4
krugo <krugovenkov@mail.ru>
2024-11-21 22:47:27 +0300

    3. ForkJoinPool [#315067 #522866]

--- src/main/java/ru/job4j/pool/ParallelArraySearch.java
+++ src/main/java/ru/job4j/pool/ParallelArraySearch.java
@@ -1,15 +1,14 @@
 package ru.job4j.pool;
 
-import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class ParallelArraySearch extends RecursiveTask<Integer> {
-    private final int value;
-    private final int[] array;
+public class ParallelArraySearch<T> extends RecursiveTask<Integer> {
+    private final T value;
+    private final T[] array;
     private final int from;
     private final int to;
 
-    public ParallelArraySearch(int value, int[] array, int from, int to) {
+    public ParallelArraySearch(T value, T[] array, int from, int to) {
         if (array.length == 0) {
             System.out.println("Array is empty");
             throw new IllegalArgumentException();
@@ -20,10 +19,10 @@ public class ParallelArraySearch extends RecursiveTask<Integer> {
         this.to = to;
     }
 
-    private int findIndex() {
-        for (int i = 0; i < array.length; i++) {
+    private int findIndex(T[] array) {
+        for (int i = from; i < to; i++) {
             if (value == array[i]) {
-                return i + from;
+                return i;
             }
         }
         return -1;
@@ -32,25 +31,15 @@ public class ParallelArraySearch extends RecursiveTask<Integer> {
     @Override
     protected Integer compute() {
         if ((to - from) <= 10) {
-            return findIndex();
+            return findIndex(array);
         }
-        int middle = (from + to) / 2;
+        var middle = (from + to) / 2;
         ParallelArraySearch firstArray = new ParallelArraySearch(value, array, from, middle);
         ParallelArraySearch secondArray = new ParallelArraySearch(value, array, middle + 1, to);
         firstArray.fork();
         secondArray.fork();
-        int first = firstArray.join();
-        int second = secondArray.join();
-        if (first == -1 && second == -1) {
-            System.out.println("value not found");
-        }
-        return first > -1 ? first : second;
-    }
-
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
-        var result = forkJoinPool.invoke(new ParallelArraySearch(7, array, 0, array.length - 1));
-        System.out.println(result);
+        Integer first = (Integer) firstArray.join();
+        Integer second = (Integer) secondArray.join();
+        return Math.max(first, second);
     }
 }
